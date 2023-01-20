313d5cf9b861c9b030e428e210eac4b2ca323937
Eric <t40933777@gmail.com>
2023-01-20 18:00:46 +0800

    帳號管理 API 串接

--- static/js/account_manager.js
+++ static/js/account_manager.js
@@ -1,7 +1,7 @@
 $(function () {
     $("#accountChange").on("click", function(e) {
       e.preventDefault(); 
-      $('input').prop("disabled",false)
+    //   $('input').prop("disabled",false)
       $('#accountConfirm').show()
     })
     $("#accountConfirm").on("click", function(e) {
@@ -11,9 +11,10 @@ $(function () {
       })
   })
 
-function batch_new(){
+export function batch_new(){
     var form = new FormData();
-    form.append("email", "yillkid@gmail.com");
+    const fileInput = document.getElementById('fileInput')
+    form.append("email", getLocalStorage("email"));
     form.append("accounts", fileInput.files[0]);
 
     var settings = {
@@ -26,6 +27,29 @@ function batch_new(){
         "data": form
       };
       $.ajax(settings).done(function (response) {
-        console.log(response);
+        let data = JSON.parse(response)
+        let subData = (data.content)
+        console.log(subData);
+        printTable(subData)
       });
 } 
+
+function printTable(subData) {
+    let tbody = document.getElementById('tbody')
+    let tr = document.getElementById('tr')
+    let td1 = document.getElementById('serialNumber')
+    let td2 = document.getElementById('userName')
+    let td3 = document.getElementById('healthNumber')
+    let td4 = document.getElementById('email')
+    subData.forEach( (data, index) => {
+        let tr = document.createElement("tr")
+		document.querySelector("#tbody").appendChild(tr)
+		let trInfo = `
+        <td id="serialNumber"> <input type="text" value="${data.device_no}" style="width: 100%;"></td>
+        <td id ="userName"> <input type="text" value="${data.account_username}" style="width: 100%;"></td>
+        <td id ="healthNumber"> <input type="text" value="待更新" style="width: 100%;"></td>
+        <td id="email"> <input type="text" value="${data.account_email}" style="width: 100%;"></td>
+		`
+		tr.innerHTML = trInfo
+	})
+}
