8ebc59b82ac325e203e50f675fb48ec88cc7ba56
yillkid <yillkid@gmail.com>
2023-02-16 20:40:31 +0800

    fix: add news API

--- static/js/cms_add_news.js
+++ static/js/cms_add_news.js
@@ -1,15 +1,19 @@
 import { news_add } from './news.js'
 
 function DataURIToBlob(dataURI) {
-  const splitDataURI = dataURI.split(',')
-  const byteString = splitDataURI[0].indexOf('base64') >= 0 ? atob(splitDataURI[1]) : decodeURI(splitDataURI[1])
-  const mimeString = splitDataURI[0].split(':')[1].split(';')[0]
+  try {
+    const splitDataURI = dataURI.split(',')
+    const byteString = splitDataURI[0].indexOf('base64') >= 0 ? atob(splitDataURI[1]) : decodeURI(splitDataURI[1])
+    const mimeString = splitDataURI[0].split(':')[1].split(';')[0]
 
-  const ia = new Uint8Array(byteString.length)
-  for (let i = 0; i < byteString.length; i++)
-      ia[i] = byteString.charCodeAt(i)
+    const ia = new Uint8Array(byteString.length)
+    for (let i = 0; i < byteString.length; i++)
+        ia[i] = byteString.charCodeAt(i)
 
-  return new Blob([ia], { type: mimeString })
+    return new Blob([ia], { type: mimeString })
+  } catch (e) {
+    return "";
+  }
 }
 
 export function set_page_info_cms_add_news() {
